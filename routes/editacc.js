adda99df8a744aea431c2406bbfbd3e3f553952e
MVolontar <44176749+MVolontar@users.noreply.github.com>
2019-05-28 16:38:52 +0200

    #2 Edit acc

--- routes/editacc.js
+++ routes/editacc.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+
 router.get('/', function(req, res, next) {
-    console.log('EDIT');
-    //first name, surname, e-mail, username, password, administration access
+    console.log(req.user.username);
+
     res.render('editacc', {
         errorMessages: 0,
         success: 0,
+        firstname: "testFistName",
+        username: "testUsername",
+        surname: "testSurname",
+        email: "testEmail",
+        username: "testUsername",
+        password: "testPass",
+        adminaccess: true,
 
     });
-
 });
-
 module.exports = router;
